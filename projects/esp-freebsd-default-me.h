1d826ae8af3ba98990696a0bac248690840f1617
Embedthis Software <dev@embedthis.com>
2015-06-11 10:43:13 -0700

    UPGRADE: paks

--- projects/esp-freebsd-default-me.h
+++ projects/esp-freebsd-default-me.h
@@ -253,6 +253,9 @@
 #ifndef ME_COM_MATRIXSSL
     #define ME_COM_MATRIXSSL 0
 #endif
+#ifndef ME_COM_MBEDTLS
+    #define ME_COM_MBEDTLS 0
+#endif
 #ifndef ME_COM_MDB
     #define ME_COM_MDB 1
 #endif
