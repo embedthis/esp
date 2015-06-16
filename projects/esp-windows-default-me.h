706358a3310c7a34ff64208c95b3909036b19916
Embedthis Software <dev@embedthis.com>
2015-06-16 14:16:24 -0700

    DOC: use iframe for version

--- projects/esp-windows-default-me.h
+++ projects/esp-windows-default-me.h
@@ -121,7 +121,7 @@
     #define ME_TUNE "speed"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "5.4.2"
+    #define ME_VERSION "5.4.3"
 #endif
 #ifndef ME_WATCHDOG_NAME
     #define ME_WATCHDOG_NAME "espman"
@@ -222,10 +222,10 @@
     #define ME_MINOR_VERSION 4
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 2
+    #define ME_PATCH_VERSION 3
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 500040002
+    #define ME_VNUM 500040003
 #endif
 
 /* Components */
