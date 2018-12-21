8eed824a3be401d275e4ce5be2dd884825a1a1de
Michael O'Brien <dev@embedthis.com>
2018-12-21 18:46:03 +1100

    REGEN

--- projects/esp-macosx-default-me.h
+++ projects/esp-macosx-default-me.h
@@ -121,7 +121,7 @@
     #define ME_TUNE "size"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "8.0.2"
+    #define ME_VERSION "8.0.3"
 #endif
 #ifndef ME_WATCHDOG_NAME
     #define ME_WATCHDOG_NAME "espman"
@@ -144,7 +144,7 @@
     #define ME_APP_PREFIX "/usr/local/lib/esp"
 #endif
 #ifndef ME_VAPP_PREFIX
-    #define ME_VAPP_PREFIX "/usr/local/lib/esp/8.0.2"
+    #define ME_VAPP_PREFIX "/usr/local/lib/esp/8.0.3"
 #endif
 #ifndef ME_BIN_PREFIX
     #define ME_BIN_PREFIX "/usr/local/bin"
@@ -177,7 +177,7 @@
     #define ME_CACHE_PREFIX "/var/spool/esp/cache"
 #endif
 #ifndef ME_SRC_PREFIX
-    #define ME_SRC_PREFIX "esp-8.0.2"
+    #define ME_SRC_PREFIX "esp-8.0.3"
 #endif
 
 /* Suffixes */
@@ -219,10 +219,10 @@
     #define ME_MINOR_VERSION 0
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 2
+    #define ME_PATCH_VERSION 3
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 800000002
+    #define ME_VNUM 800000003
 #endif
 
 /* Components */
--- projects/esp-vxworks-default-me.h
+++ projects/esp-vxworks-default-me.h
@@ -121,7 +121,7 @@
     #define ME_TUNE "size"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "8.0.2"
+    #define ME_VERSION "8.0.3"
 #endif
 #ifndef ME_WATCHDOG_NAME
     #define ME_WATCHDOG_NAME "espman"
@@ -177,7 +177,7 @@
     #define ME_VAPP_PREFIX "deploy"
 #endif
 #ifndef ME_SRC_PREFIX
-    #define ME_SRC_PREFIX "/usr/src/esp-8.0.2"
+    #define ME_SRC_PREFIX "/usr/src/esp-8.0.3"
 #endif
 
 /* Suffixes */
@@ -219,10 +219,10 @@
     #define ME_MINOR_VERSION 0
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 2
+    #define ME_PATCH_VERSION 3
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 800000002
+    #define ME_VNUM 800000003
 #endif
 
 /* Components */
