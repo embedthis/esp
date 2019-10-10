93100f35f9482714e3eb5cb11eaac3a6f86959b9
Michael O'Brien <dev@embedthis.com>
2019-10-10 17:21:21 +1100

    DEV: bump version and regen

--- projects/esp-linux-default-me.h
+++ projects/esp-linux-default-me.h
@@ -130,7 +130,7 @@
     #define ME_TUNE "size"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "8.1.0"
+    #define ME_VERSION "8.1.1"
 #endif
 #ifndef ME_WATCHDOG_NAME
     #define ME_WATCHDOG_NAME "espman"
@@ -153,7 +153,7 @@
     #define ME_APP_PREFIX "/usr/local/lib/esp"
 #endif
 #ifndef ME_VAPP_PREFIX
-    #define ME_VAPP_PREFIX "/usr/local/lib/esp/8.1.0"
+    #define ME_VAPP_PREFIX "/usr/local/lib/esp/8.1.1"
 #endif
 #ifndef ME_BIN_PREFIX
     #define ME_BIN_PREFIX "/usr/local/bin"
@@ -186,7 +186,7 @@
     #define ME_CACHE_PREFIX "/var/spool/esp/cache"
 #endif
 #ifndef ME_SRC_PREFIX
-    #define ME_SRC_PREFIX "esp-8.1.0"
+    #define ME_SRC_PREFIX "esp-8.1.1"
 #endif
 
 /* Suffixes */
@@ -228,10 +228,10 @@
     #define ME_MINOR_VERSION 1
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 0
+    #define ME_PATCH_VERSION 1
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 800010000
+    #define ME_VNUM 800010001
 #endif
 
 /* Components */
--- projects/esp-vxworks-default-me.h
+++ projects/esp-vxworks-default-me.h
@@ -130,7 +130,7 @@
     #define ME_TUNE "size"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "8.1.0"
+    #define ME_VERSION "8.1.1"
 #endif
 #ifndef ME_WATCHDOG_NAME
     #define ME_WATCHDOG_NAME "espman"
@@ -186,7 +186,7 @@
     #define ME_VAPP_PREFIX "deploy"
 #endif
 #ifndef ME_SRC_PREFIX
-    #define ME_SRC_PREFIX "/usr/src/esp-8.1.0"
+    #define ME_SRC_PREFIX "/usr/src/esp-8.1.1"
 #endif
 
 /* Suffixes */
@@ -228,10 +228,10 @@
     #define ME_MINOR_VERSION 1
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 0
+    #define ME_PATCH_VERSION 1
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 800010000
+    #define ME_VNUM 800010001
 #endif
 
 /* Components */
