225472debbc9b14883bd03e767746c7e7e9c1f32
Michael O'Brien <dev@embedthis.com>
2020-11-26 15:13:57 +1100

    DEV: update paks

--- projects/esp-windows-default-me.h
+++ projects/esp-windows-default-me.h
@@ -136,7 +136,7 @@
     #define ME_TUNE "size"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "9.0.0"
+    #define ME_VERSION "9.0.1"
 #endif
 #ifndef ME_WATCHDOG_NAME
     #define ME_WATCHDOG_NAME "espman"
@@ -237,10 +237,10 @@
     #define ME_MINOR_VERSION 0
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 0
+    #define ME_PATCH_VERSION 1
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 900000000
+    #define ME_VNUM 900000001
 #endif
 
 /* Components */
